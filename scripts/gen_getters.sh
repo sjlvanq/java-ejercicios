904460d56e3774f33fbfe767a3a2baa309345ed0
sjlvanq <sjlvanq@gmail.com>
2025-04-09 05:01:24 -0300

    crear scripts/gen_setters.sh y scripts/gen_getters.sh

---
+++ scripts/gen_getters.sh
@@ -0,0 +1,25 @@
+#!/bin/env bash
+set -e
+
+usage() {
+  cat <<EOF
+Uso: $(basename "${BASH_SOURCE[0]}") [-h] nombre_archivo
+EOF
+  exit
+}
+
+if [ $# -eq 0 ] || [ $1 = '-h' ]; then usage; fi
+if [ ! -f $1 ]; then echo "Fichero inexistente"; usage; fi
+
+ctags -x $1 | sed -E 's/[[:space:]]+/\;/g' | awk -F ';' '{
+	if ($2 == "field")
+	{
+		attrib=$1
+		attribcaps=toupper(substr(attrib,1,1))substr(attrib,2,length(attrib))
+		type=$(NF-2)
+		print "public",type,"get"attribcaps"() {"
+		print "\treturn this."attrib";"
+		print "}"
+	}
+}'
+
