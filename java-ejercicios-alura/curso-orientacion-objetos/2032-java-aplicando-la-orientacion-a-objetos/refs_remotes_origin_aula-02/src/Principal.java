9754f7b69f1940fb7230c9c8e7cdd277f8779e32
sjlvanq <sjlvanq@gmail.com>
2025-04-15 16:20:09 -0300

    crear java-ejercicios-alura/curso-orientacion-objetos/2032-java-aplicando-la-orientacion-a-objetos/

---
+++ java-ejercicios-alura/curso-orientacion-objetos/2032-java-aplicando-la-orientacion-a-objetos/refs_remotes_origin_aula-02/src/Principal.java
@@ -0,0 +1,31 @@
+import com.aluracursos.screenmatch.modelos.Pelicula;
+
+public class Principal {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.setNombre("Encanto");
+        miPelicula.setFechaDeLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(120);
+        miPelicula.setIncluidoEnElPlan(true);
+
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+        miPelicula.evalua(7.8);
+        System.out.println(miPelicula.getTotalDelasEvaluaciones());
+        System.out.println(miPelicula.calculaMedia());
+
+
+
+
+
+
+
+
+//        Pelicula otraPelicula = new Pelicula();
+//        otraPelicula.setNombre("Matrix");
+//        otraPelicula.setFechaDeLanzamiento(1998);
+//        otraPelicula.setDuracionEnMinutos(180);
+
+    }
+}
---
+++ java-ejercicios-alura/curso-orientacion-objetos/2032-java-aplicando-la-orientacion-a-objetos/refs_remotes_origin_aula-03/src/Principal.java
@@ -0,0 +1,46 @@
+import com.aluracursos.screenmacth.calculos.CalculadoraDeTiempo;
+import com.aluracursos.screenmatch.modelos.Pelicula;
+import com.aluracursos.screenmatch.modelos.Serie;
+
+public class Principal {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.setNombre("Encanto");
+        miPelicula.setFechaDeLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(120);
+        miPelicula.setIncluidoEnElPlan(true);
+
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+        miPelicula.evalua(7.8);
+        System.out.println(miPelicula.getTotalDelasEvaluaciones());
+        System.out.println(miPelicula.calculaMedia());
+
+        Serie casaDragon = new Serie();
+        casaDragon.setNombre("La casa del dragón");
+        casaDragon.setFechaDeLanzamiento(2022);
+        casaDragon.setTemporadas(1);
+        casaDragon.setMinutosPorEpisodio(50);
+        casaDragon.setEpisodiosPorTemporada(10);
+        casaDragon.muestraFichaTecnica();
+        System.out.println(casaDragon.getDuracionEnMinutos());
+
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.setNombre("Matrix");
+        otraPelicula.setFechaDeLanzamiento(1998);
+        otraPelicula.setDuracionEnMinutos(180);
+
+        CalculadoraDeTiempo calculadora = new CalculadoraDeTiempo();
+        calculadora.incluye(miPelicula);
+        calculadora.incluye(casaDragon);
+        calculadora.incluye(otraPelicula);
+        System.out.println("Tiempo necesario para ver tus títulos favoritos estas vacaciones "
+                + calculadora.getTiempoTotal() + " minutos");
+
+
+
+
+
+    }
+}
