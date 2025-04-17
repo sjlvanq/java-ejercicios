ef9e2813786435682568e21aceb73db52fa3d56c
sjlvanq <sjlvanq@gmail.com>
2025-04-17 13:20:40 -0300

    Evitar retorno directo de la lista de compras (resuelve #2)

--- java-ejercicios-alura/curso-listas-colecciones/E0500/models/Monedero.java
+++ java-ejercicios-alura/curso-listas-colecciones/E0500/models/Monedero.java
@@ -1,10 +1,11 @@
 package models;
 
+import java.util.List;
 import java.util.ArrayList;
 
 public class Monedero {
 	private static int monedas;
-	private static ArrayList<Producto> compras = new ArrayList<>();
+	private static List<Producto> compras = new ArrayList<>();
 	
 	public static void comprar(Producto producto) {
 		if(producto.getPrecio() <= Monedero.monedas) {
@@ -21,8 +22,10 @@ public class Monedero {
 		return monedas;
 	}
 	
-	public static ArrayList<Producto> getCompras(){
-		return compras;
+	public static List<Producto> getCompras(){
+		//return compras; // Devuelve referencia
+		//return List.copyOf(compras); // Devuelve lista inmutable
+		return new ArrayList<>(compras);
 	}
 
 	public static boolean meAlcanzaParaAlgo(Tienda tienda) {
