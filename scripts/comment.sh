ef70d1f9d6f0222132054e989a878d703043a6f5
sjlvanq <sjlvanq@gmail.com>
2025-04-18 13:50:31 -0300

    crear scripts/comment.sh

---
+++ scripts/comment.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+if [ $# -eq 0 ]; then 
+	echo "Produce un comentario de bloque con extensión total de 80 líneas"
+	echo "USO: $0 texto_a_comentar"; exit; 
+fi
+
+echo '/*'
+echo $1 | fold -sw 77 | sed s/'^'/' * '/g
+echo ' */'
