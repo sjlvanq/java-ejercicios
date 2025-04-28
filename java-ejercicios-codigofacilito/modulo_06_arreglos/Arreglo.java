a87154e569c71738500237fa0db6723921d883a8
sjlvanq <sjlvanq@gmail.com>
2025-04-28 15:03:54 -0300

    actualizar Arreglo.java: usar Arrays.stream en max, min y prom

--- java-ejercicios-codigofacilito/modulo_06_arreglos/Arreglo.java
+++ java-ejercicios-codigofacilito/modulo_06_arreglos/Arreglo.java
@@ -12,19 +12,41 @@ public class Arreglo
 			System.out.print("%d ".formatted(item));
 		}
 		System.out.println();
-				
+
 		// Mostrar en consola el número mayor.
+
+		/*
 		int mayor = 0;
 		for (int item : miArreglo) {
 			if (item > mayor) {mayor = item;}
-		} System.out.printf("Max: %d%n".formatted(mayor));
-		
+		}
+		*/
+
+		// max() retorna un OptionalInt
+		// incompatible types: OptionalInt cannot be converted to int
+		// entonces, getAsInt()
+		// https://docs.oracle.com/javase/8/docs/api/java/util/OptionalInt.html
+
+		int mayor = Arrays.stream(miArreglo).max().getAsInt();
+		System.out.printf("Max: %d%n".formatted(mayor));
+
 		// Mostrar en consola el número menor.
+
+		/*
 		int menor = 100;
 		for (int item : miArreglo) {
 			if (item < menor) {menor = item;}
-		} System.out.printf("Min: %d%n".formatted(menor));
-		
+		}
+		*/
+
+		// max() retorna un OptionalInt
+		// incompatible types: OptionalInt cannot be converted to int
+		// entonces, getAsInt()
+		// https://docs.oracle.com/javase/8/docs/api/java/util/OptionalInt.html
+
+		int menor = Arrays.stream(miArreglo).min().getAsInt();
+		System.out.printf("Min: %d%n".formatted(menor));
+
 		// Mostrar en consola todos los números primos.
 		System.out.print("Primos: ");
 		int i;
@@ -36,7 +58,7 @@ public class Arreglo
 				System.out.printf("%d ".formatted(item));
 			}
 		} System.out.println();
-		
+
 		// Mostrar en consola todos los números pares.
 		System.out.print("Pares: ");
 		for (int item : miArreglo) { 
@@ -45,22 +67,32 @@ public class Arreglo
 				continue;
 			}
 		} System.out.println();
-		
+
 		// Mostrar en consola el arreglo de forma ascendente.
 		Arrays.sort(miArreglo);
 		System.out.print("Ordenado: ");
 		for (int item : miArreglo) {
 			System.out.printf("%d ", item);
 		} System.out.println();
-		
+
 		// Mostrar en consola el promedio del arreglo.
+
+		/*
 		int sum=0;
 		for (int item : miArreglo) {sum+=item;}
 		System.out.println("Promedio: %f".formatted((float) sum / (float) miArreglo.length));
-		
+		*/
+
+		// average() retorna un OptionalDouble
+		// entonces, getAsDouble()
+		// https://docs.oracle.com/javase/8/docs/api/java/util/OptionalDouble.html
+
+		double average = Arrays.stream(miArreglo).average().getAsDouble();
+		System.out.println("Promedio: %f".formatted(average));
+
 		// Mostrar en consola la suma del primer y último elemento del arreglo.
 		System.out.println("Primero + último: %d".formatted(miArreglo[0]+miArreglo[miArreglo.length-1]));
-		
+
 		// Mostrar en consola el mensaje "Existe cien" en dado caso el arreglo almacene por los menos un cien.
 		
 		for (int item : miArreglo) {
