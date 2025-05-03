33fe435480df431a962753f1ac118bff4feb0922
sjlvanq <sjlvanq@gmail.com>
2025-05-03 14:41:01 -0300

    borrar /java-pruebas y /test

--- java-pruebas/ListCopyOfB.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class ListCopyOfB
-{
-	public static ArrayList<Integer> lista = new ArrayList<>();
-	public static ArrayList<Integer> getLista(){
-		return lista;
-	}
-}
